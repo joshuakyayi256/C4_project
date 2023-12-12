9efe8bebcb5178899e136b9fd8b4317c73ab6ab2
Magero Joshua <kyayijoshua@gmail.com>
2023-12-12 12:03:45 +0300

    ROUTES

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {Link} from 'react-router-dom';
-import { Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import SignUp from './components/SignUp';
 import Recovery from './components/Recovery';
 
 function App() {
   return (
-  //  <Login />
-  // <SignUp />
-  <Recovery />
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/recovery" element={<Recovery />} />
+      </Routes>
+    </>
   );
 }
 
